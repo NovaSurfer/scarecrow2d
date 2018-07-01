1113c7b9dae1afb151f955eacc5b6c2b0931831c
Maksim Ruts <tribar.ge@gmail.com>
2018-07-01 23:06:16 +0300

    Adds Catch test framework

---
+++ tests/math_tests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by maksim on 6/29/18.
+//
+#include "catch2/catch.hpp"
+
+// TODO: -------------------
+TEST_CASE("vec2d-scalar-multiply")
+{
+    REQUIRE( true );
+}
---
+++ tests/unit.cpp
@@ -0,0 +1,6 @@
+//
+// Created by maksim on 6/29/18.
+//
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
