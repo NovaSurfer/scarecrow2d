bfccbe945465bbb3843eeecfcdf55121fd6fa9a6
Maksim Ruts <tribar.ge@gmail.com>
2018-07-10 23:21:35 +0300

    adds support for textures that can be resized or rotated

--- src/textureManager.h
+++ src/textureManager.h
@@ -6,14 +6,18 @@
 #define INC_2D_ENGINE_TEXTUREMANAGER_H
 
 #include "game.h"
+#include "math/matrices.h"
 #include <iostream>
 
 
 class TextureManager {
 public:
     static SDL_Texture *load_texture(const char *texFile);
-
+    static std::pair<SDL_Texture *, SDL_Surface *> load_texture_resizable(const char *texFile);
     static void draw_texture(SDL_Texture *texture, SDL_Rect src, SDL_Rect dest);
+    static void draw_texture(SDL_Texture *texture, const SDL_Rect &srcrect, const SDL_Rect &destrect,
+                             const double angle, const SDL_Point &center, const SDL_RendererFlip flip);
+
 };
 
 
