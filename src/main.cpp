4d1c3080bc3777fda0d551fe5de0deb8d16816e7
Maksim Ruts <tribar.ge@gmail.com>
2018-12-23 21:07:43 +0300

    renamed

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,108 @@
-#include "game.h"
+//
+// Created by Maksim Ruts on 28-Aug-18.
+//
 
-#undef main
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "window.h"
+#include "filesystem/configLoader.h"
+#include "core/logger.h"
+
+std::unique_ptr<Window> window;
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+bool engine_init()
+{
+    return Config<ResourcesConfigLoad>::open("resources.json");
+}
+
+int init()
+{
+    glfwInit();
+
+    const WindowData window_data{3, 3, GLFW_OPENGL_CORE_PROFILE, 800, 600, "scarecrow2d", framebuffer_size_callback, key_callback};
+    window = std::make_unique<Window>(window_data, true);
+
+    // Glad loads OpenGL functions pointers
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Loading engine systems
+    if (!engine_init()) return -2;
+
+    return glGetError();
+}
+
+void poll_events()
+{
+    glfwPollEvents();
+}
+
+void draw()
+{
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window->get_window());
+}
+
+void update(double dt)
+{
+
+}
 
 int main()
 {
-    // TODO: fix main loop
-    constexpr uint32_t FPS = 60;
-    constexpr uint32_t frameDelay = 1000 / FPS;
-    uint32_t frameStart = 0;
-    int frameTime = 0;
+    if(init() != 0)
+        return -1;
 
-    std::unique_ptr<Game> game = std::make_unique<Game>();
-    game->init("2D Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+    // TODO: Make class for time management
+    double begin_ticks = glfwGetTime();
+    double end_ticks = 0;
+    double delta_time = 1.0 / 60.0;
 
-    while (game->running())
-    {
-        frameStart = SDL_GetTicks();
+    // Game loop
+    while (!glfwWindowShouldClose(window->get_window()))
+    {   
+        update(delta_time);
+        poll_events();
+        draw();
+        
+        end_ticks = glfwGetTime();
+        delta_time = (begin_ticks - end_ticks) / glfwGetTime();
 
-        game->handle_events();
-        game->update();
-        game->render();
-        frameTime = SDL_GetTicks() - frameStart;
-
-        if (frameDelay > frameTime)
+    #ifdef NDEBUG
+        // If delta_time is too large, we must have resumed from a 
+        // breakpoint -- frame-lock to the target rate this frame.
+        if(delta_time > 1.0)
         {
-            SDL_Delay(frameDelay - frameTime);
-
+            delta_time = 1.0 / 60.0
         }
+    #endif
+
+        begin_ticks = end_ticks;
     }
 
-    game->clean();
+    glfwTerminate();
     return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    // When a user presses the escape key, we set the WindowShouldClose property to true, closing the application
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
 }
