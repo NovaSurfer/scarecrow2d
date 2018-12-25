019ef92e16416b296f40973c785aa211a744e9ad
Maksim Ruts <tribar.ge@gmail.com>
2018-12-25 14:40:40 +0300

    moving outside of struct, removing friend keyword

--- src/math/vector.h
+++ src/math/vector.h
@@ -56,98 +56,98 @@ namespace math {
             return (*this);
         }
 
-        friend bool operator==(const vec2 &a, const vec2 &b)
-        {
-            return math::utils::cmp(a.x, b.x) && math::utils::cmp(a.y, b.y);
-        }
-
-        friend bool operator!=(const vec2 &a, const vec2 &b)
-        {
-            return !(a == b);
-        }
-
-        friend vec2 operator*(const vec2 &v, float s)
-        {
-            return (vec2(v.x * s, v.y * s));
-        }
-
-        friend vec2 operator/(const vec2 &v, float s)
-        {
-            s = 1.0f / s;
-            return (vec2(v.x * s, v.y * s));
-        }
-
-        friend vec2 operator-(const vec2 &v)
-        {
-            return (vec2(-v.x, -v.y));
-        }
-
-        friend vec2 operator+(const vec2 &a, const vec2 &b)
-        {
-            return (vec2(a.x + b.x, a.y + b.y));
-        }
-
-        friend vec2 operator-(const vec2 &a, const vec2 &b)
-        {
-            return (vec2(a.x - b.x, a.y - b.y));
-        }
-
-        friend float dot(const vec2 &a, const vec2 &b)
-        {
-            return (a.x * b.x + a.y * b.y);
-        }
-
-        friend float magnitude(const vec2 &v)
-        {
-            return (std::sqrt(dot(v, v)));
-        }
-
-        friend float magnitudeSq(const vec2 &v)
-        {
-            return dot(v, v);
-        }
-
-        friend float distance(const vec2 &a, const vec2 &b)
-        {
-            vec2 t = a - b;
-            return magnitude(t);
-        }
-
-        friend vec2 normalize(const vec2 &v)
-        {
-            return (v / magnitude(v));
-        }
-
-        friend float angle(const vec2 &a, const vec2 &b)
-        {
-            float m = std::sqrt(magnitudeSq(a) * magnitudeSq(b));
-            return std::acos(dot(a, b) / m);
-        }
-
-        friend vec2 project(const vec2 &len, const vec2 &dir)
-        {
-            float d = dot(len, dir);
-            float magSq = magnitudeSq(dir);
-            return dir * (d / magSq);
-        }
-
-        friend vec2 perpendicular(const vec2 &len, const vec2 &dir)
-        {
-            return len - project(len, dir);
-        }
-
-        friend vec2 reflection(const vec2 &v, const vec2 &normal)
-        {
-            float d = dot(v, normal);
-            return v - normal * (d * 2.0f);
-        }
-
         friend std::ostream &operator<<(std::ostream &ostream, const vec2 &v)
         {
             ostream << "(" << v.x << "," << v.y << ")";
             return ostream;
         }
     };
+
+    bool operator==(const vec2 &a, const vec2 &b)
+    {
+        return math::utils::cmp(a.x, b.x) && math::utils::cmp(a.y, b.y);
+    }
+
+    bool operator!=(const vec2 &a, const vec2 &b)
+    {
+        return !(a == b);
+    }
+
+    vec2 operator*(const vec2 &v, float s)
+    {
+        return (vec2(v.x * s, v.y * s));
+    }
+
+    vec2 operator/(const vec2 &v, float s)
+    {
+        s = 1.0f / s;
+        return (vec2(v.x * s, v.y * s));
+    }
+
+    vec2 operator-(const vec2 &v)
+    {
+        return (vec2(-v.x, -v.y));
+    }
+
+    vec2 operator+(const vec2 &a, const vec2 &b)
+    {
+        return (vec2(a.x + b.x, a.y + b.y));
+    }
+
+    vec2 operator-(const vec2 &a, const vec2 &b)
+    {
+        return (vec2(a.x - b.x, a.y - b.y));
+    }
+
+    float dot(const vec2 &a, const vec2 &b)
+    {
+        return (a.x * b.x + a.y * b.y);
+    }
+
+    float magnitude(const vec2 &v)
+    {
+        return (std::sqrt(dot(v, v)));
+    }
+
+    float magnitudeSq(const vec2 &v)
+    {
+        return dot(v, v);
+    }
+
+    float distance(const vec2 &a, const vec2 &b)
+    {
+        vec2 t = a - b;
+        return magnitude(t);
+    }
+
+    vec2 normalize(const vec2 &v)
+    {
+        return (v / magnitude(v));
+    }
+
+    float angle(const vec2 &a, const vec2 &b)
+    {
+        float m = std::sqrt(magnitudeSq(a) * magnitudeSq(b));
+        return std::acos(dot(a, b) / m);
+    }
+
+    vec2 project(const vec2 &len, const vec2 &dir)
+    {
+        float d = dot(len, dir);
+        float magSq = magnitudeSq(dir);
+        return dir * (d / magSq);
+    }
+
+    vec2 perpendicular(const vec2 &len, const vec2 &dir)
+    {
+        return len - project(len, dir);
+    }
+
+    vec2 reflection(const vec2 &v, const vec2 &normal)
+    {
+        float d = dot(v, normal);
+        return v - normal * (d * 2.0f);
+    }
 };
 
 
