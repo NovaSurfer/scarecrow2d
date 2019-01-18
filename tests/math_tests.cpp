906e6099bdeeb4dbd66948bfd6ba8093cb485776
Maksim Ruts <tribar.ge@gmail.com>
2019-01-19 00:42:54 +0300

    adds tests for 4x4 matrix

--- tests/math_tests.cpp
+++ tests/math_tests.cpp
@@ -5,6 +5,7 @@
 #include "catch2/catch.hpp"
 #include "../src/math/matrix2.h"
 #include "../src/math/matrix3.h"
+#include "../src/math/matrix4.h"
 #include "../src/math/vector2.h"
 
 using namespace math;
@@ -115,6 +116,9 @@ TEST_CASE("mat2d-operations", "[math]")
     }
 }
 
+// -------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------
+
 TEST_CASE("mat3d-operations", "[math]")
 {
     mat3 matrix(1, 1, 1, 2, 2, 2, 3, 3, 3);
@@ -223,6 +227,133 @@ TEST_CASE("mat3d-operations", "[math]")
     {
         REQUIRE(matx3.adjugate() == mat3(-9, 0, 3, 66, -6, -6, -39, 6, 3));
     }
+    // TODO:
+    // SECTION("inverse")
+    // {
+    //     mat3 matrix2(1, 1, 1, 2, 5, 8, 9, 3, 3);
+    //     std::cout << matrix2.inverse();
+    //     REQUIRE(matrix2 * matrix2.inverse() == mat3(1, 0, 0, 0, 1, 0, 0, 0, 1));
+    // }
+}
+
+// -------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------
+
+TEST_CASE("mat4d-operations", "[math]")
+{
+    mat4 matrix(1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 5, 8, 9, 4, 4, 4);
+    mat4 matx3(1, 1, 2, 1, 2, 4, 2, 2, 2, 5, 5, 8, 9, 4, 4, 4);
+
+    SECTION("is equal")
+    {
+        mat4 m(1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 5, 8, 9, 4, 4, 4);
+        REQUIRE(matrix == m);
+    }
+
+    SECTION("get value at position (i,j)")
+    {
+        REQUIRE(matrix(0, 0) == 1);
+        REQUIRE(matrix(0, 1) == 1);
+        REQUIRE(matrix(0, 2) == 1);
+        REQUIRE(matrix(0, 3) == 1);
+
+        REQUIRE(matrix(1, 0) == 2);
+        REQUIRE(matrix(1, 1) == 2);
+        REQUIRE(matrix(1, 2) == 2);
+        REQUIRE(matrix(1, 3) == 2);
+
+        REQUIRE(matrix(2, 0) == 3);
+        REQUIRE(matrix(2, 1) == 3);
+        REQUIRE(matrix(2, 2) == 5);
+        REQUIRE(matrix(2, 3) == 8);
+
+        REQUIRE(matrix(3, 0) == 9);
+        REQUIRE(matrix(3, 1) == 4);
+        REQUIRE(matrix(3, 2) == 4);
+        REQUIRE(matrix(3, 3) == 4);
+
+        SECTION("get values at position as const")
+        {
+            const mat4 const_matrix;
+            REQUIRE(std::is_const<std::remove_reference<decltype(const_matrix(0, 0))>::type>::value);
+        }
+    }
+
+    SECTION("get column values in vec4")
+    {
+        REQUIRE(typeid(matrix[0]) == typeid(vec4));
+
+        SECTION("get column values as const")
+        {
+            const mat4 const_matrix;
+            REQUIRE(std::is_const<std::remove_reference<decltype(const_matrix[0])>::type>::value);
+        }
+    }
+
+    SECTION("scalar multiplication")
+    {
+        float scalar = 8;
+        mat4 result = matrix * 8;
+
+        REQUIRE(result == mat4(8,8,8,8,16,16,16,16,24,24,40,64,72,32,32,32));
+
+        SECTION("*= operator with scalar")
+        {
+            matrix *= 8;
+            REQUIRE(matrix == mat4(8,8,8,8,16,16,16,16,24,24,40,64,72,32,32,32));
+        }
+    }
+
+    SECTION("matrix multiplication")
+    {
+        mat4 matrix2(1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4);
+        mat4 result = matrix * matrix2;
+        REQUIRE(result == mat4(10,10,10,10,20,20,20,20,56,56,56,56,45,45,45,45));
+
+        SECTION("*= operator with matrix")
+        {
+            matrix *= matrix2;
+            REQUIRE(matrix == mat4(10,10,10,10,20,20,20,20,56,56,56,56,45,45,45,45));
+        }
+    }
+
+    SECTION("matrix addition")
+    {
+        mat4 matrix2(1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4);
+        mat4 result = matrix + matrix2;
+        REQUIRE(result == mat4(2, 2, 2, 2, 4, 4, 4, 4, 6, 6, 8, 11, 13, 8, 8, 8));
+
+        SECTION("+= operator with matrix")
+        {
+            matrix += matrix2;
+            REQUIRE(matrix == mat4(2, 2, 2, 2, 4, 4, 4, 4, 6, 6, 8, 11, 13, 8, 8, 8));
+        }
+    }
+
+    SECTION("transpose")
+    {
+        REQUIRE(matrix.transpose() == mat4(1, 2, 3, 9, 1, 2, 3, 4, 1, 2, 5, 4, 1, 2, 8, 4));
+    }
+// TODO:
+//    SECTION("determinant")
+//    {
+//        REQUIRE(matx3.determinant() == 188);
+//    }
+
+    SECTION("minor")
+    {
+        REQUIRE(matx3.minor() == mat4(-24, 30, 158, 74, 12, 79, 15, -37, -8, 10, -10, -38, -28, -12, 12, 8));
+    }
+
+    SECTION("cofactor")
+    {
+        REQUIRE(matx3.cofactor() == mat4(-24, -30, 158, -74, -12, 79, -15, -37, -8, -10, -10, 38, 28, -12, -12, 8));
+    }
+
+    SECTION("adjugate")
+    {
+        REQUIRE(matx3.adjugate() == mat4(-24, -12, -8, 28, -30, 79, -10, -12, 158, -15, -10, -12, -74, -37, 38, 8));
+    }
 
     // SECTION("inverse")
     // {
@@ -231,6 +362,7 @@ TEST_CASE("mat3d-operations", "[math]")
     //     REQUIRE(matrix2 * matrix2.inverse() == mat3(1, 0, 0, 0, 1, 0, 0, 0, 1));
     // }
 }
+
 
 TEST_CASE("vec2d-operations", "[math]")
 {
