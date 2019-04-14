14829b1ad101f49b1859549472f9c10cb5394733
Maksim Ruts <tribar.ge@gmail.com>
2019-04-14 22:10:37 +0300

    Adds program memory initializer

---
+++ src/memory/memory.cpp
@@ -0,0 +1,9 @@
+// Copyright (C) 2012 Bitsquid AB
+// License: https://bitbucket.org/bitsquid/foundation/src/default/LICENCSE
+
+#include "memory"
+
+namespace sc2d::memory::main
+{
+
+}
---
+++ src/memory/memory.h
@@ -0,0 +1,14 @@
+//
+// Created by Maksim on 4/8/2019.
+//
+
+#ifndef INC_2D_GAME_MEMORY_H
+#define INC_2D_GAME_MEMORY_H
+
+namespace sc2d::memory::main
+{
+    void init();
+    void destroy();
+}
+
+#endif //INC_2D_GAME_MEMORY_H
