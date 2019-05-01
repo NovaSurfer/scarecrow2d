45d2e691e6409453ea5390770edf535b94e10f3d
Maksim Ruts <tribar.ge@gmail.com>
2019-05-01 21:44:26 +0300

    Adds new fixed size pool (WIP)

--- src/memory/pool_allocator.h
+++ src/memory/pool_allocator.h
@@ -6,24 +6,31 @@
 #define INC_2D_GAME_POOL_ALLOCATOR_H
 
 #include "allocator.h"
+#include "aligned_allocator.h"
 
 namespace sc2d::memory {
 
-    class pool_allocator : public allocator {
+    class pool_allocator {
     public:
+        ~pool_allocator() {destroy();}
 
-        pool_allocator(size_t chunk_size, uint8_t chunk_alignment, size_t size, void* mem);
+        void create(size_t block_size, size_t num_of_blocks, size_t alignment);
+        void destroy();
+        void* allocate();
+        void deallocate(void* ptr);
 
-        ~pool_allocator();
-
-        void* allocate(size_t size, uint8_t alignment) override;
-        void deallocate(void* p) override;
-
+        size_t num_of_blocks = 0;
+        size_t size_of_block = 0;
+        size_t num_of_free_blocks = 0;
+        size_t num_of_initialized = 0;
+        unsigned char* p_start = nullptr;
+        unsigned char* p_next = nullptr;
     private:
-        size_t chunk_size;
-        uint8_t chunk_alignment;
-        void**  pool_list = nullptr;
+        void* addr_from_index(size_t index) const;
+
+        size_t index_from_addr(const unsigned char* ptr) const;
     };
+
 }
 
 #endif //INC_2D_GAME_POOL_ALLOCATOR_H
