4cb0e70018a17fe95b1cc1311bc15590c7541e84
Maksim Ruts <tribar.ge@gmail.com>
2019-06-04 23:58:23 +0300

    Adds vec<T> class to friends

--- src/memory/pool_allocator.h
+++ src/memory/pool_allocator.h
@@ -8,34 +8,46 @@
 #include "allocator.h"
 #include "aligned_allocator.h"
 
-namespace sc2d::memory {
 
-    class pool_allocator {
-    public:
-        ~pool_allocator() {destroy();}
+namespace sc2d{
 
-        void create(size_t block_size, size_t num_of_blocks, size_t alignment);
-        void destroy();
-        void* allocate();
-        void resize(size_t new_size);
-        void deallocate(void* ptr);
+    // Forward declaring vec<T> class
+    template <typename T>
+    class vec;
 
-        unsigned char* get_start() const { return p_start;}
-        size_t get_intialized_num() const { return num_of_initialized; }
-        size_t get_num_of_blocks() const {return num_of_blocks; }
+    namespace memory {
 
-    private:
-        unsigned char* addr_from_index(size_t index) const;
-        size_t index_from_addr(const unsigned char* ptr) const;
+        class pool_allocator {
 
-        size_t num_of_blocks = 0;
-        size_t size_of_block = 0;
-        size_t num_of_free_blocks = 0;
-        size_t num_of_initialized = 0;
-        unsigned char* p_start = nullptr;
-        unsigned char* p_next = nullptr;
-    };
+            template <typename T>
+            friend class sc2d::vec;
 
+        public:
+            ~pool_allocator() {destroy();}
+
+            void create(size_t block_size, size_t num_of_blocks, size_t alignment);
+            void destroy();
+            void* allocate();
+            void resize(size_t new_size);
+            void deallocate(void* ptr);
+
+            unsigned char* get_start() const { return p_start;}
+            size_t get_intialized_num() const { return num_of_initialized; }
+            size_t get_num_of_blocks() const {return num_of_blocks; }
+
+        private:
+            unsigned char* addr_from_index(size_t index) const;
+            size_t index_from_addr(const unsigned char* ptr) const;
+
+            size_t num_of_blocks = 0;
+            size_t size_of_block = 0;
+            size_t num_of_free_blocks = 0;
+            size_t num_of_initialized = 0;
+            unsigned char* p_start = nullptr;
+            unsigned char* p_next = nullptr;
+        };
+
+    }
 }
 
 #endif //INC_2D_GAME_POOL_ALLOCATOR_H
