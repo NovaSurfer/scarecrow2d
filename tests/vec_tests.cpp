fad2f25fe6e6b0bcd8df693aef3df6b9a1c544ce
Maksim Ruts <tribar.ge@gmail.com>
2019-06-16 22:51:19 +0300

    Adds tests for vector

--- tests/vec_tests.cpp
+++ tests/vec_tests.cpp
@@ -1,39 +1,92 @@
 //
 // Created by maksim.ruts on 3.4.19.
 //
-#include <type_traits>
 #include "catch2/catch.hpp"
+#include "../src/memory/pool_allocator.h"
 #include "../src/collections/vec.h"
 
-TEST_CASE("vector-operations", "[collections]")
-{
-    struct trivial_type
+
+TEST_CASE("vector-operations", "[collections]") {
+//    struct trivial_type {
+//        int int_a = 9;
+//        char char_a = 'z';
+//        bool bool_a = false;
+//    };
+//
+//    class non_trivial_type {
+//    public:
+//        non_trivial_type() {
+//            int_a = 8;
+//            char_a = 'm';
+//            bool_a = true;
+//        }
+//
+//    private:
+//        int int_a;
+//        char char_a;
+//        bool bool_a;
+//    };
+//
+//    trivial_type trivial_obj;
+//    non_trivial_type non_trivial_obj;
+
+    SECTION("default constructor")
     {
-        int int_a = 9;
-        char char_a = 'z';
-        bool bool_a = false;
-    };
+        sc2d::vec<double> v;
+        REQUIRE(v.capacity() == 2);
+    }
 
-    class non_trivial_type
+    SECTION("constructor with size")
     {
-    public:
-        non_trivial_type()
-        {
-            int_a = 8;
-            char_a = 'm';
-            bool_a = true;
-        }
+        sc2d::vec<double> v(2);
+        REQUIRE(v.capacity() == 4);
+    }
 
-    private:
-        int int_a;
-        char char_a;
-        bool bool_a;
-    };
+    SECTION("constructor with size, filled with some value")
+    {
+        double value = 5.799;
+        sc2d::vec<double> v(4, value);
 
-    trivial_type trivial_obj;
-    non_trivial_type non_trivial_obj;
+        REQUIRE(v.capacity() == 8);
+        REQUIRE(v[0] == 5.799);
+        REQUIRE(v[1] == 5.799);
+        REQUIRE(v[2] == 5.799);
+        REQUIRE(v[3] == 5.799);
+    }
 
-//    sc2d::vec<double> triv_objs;
-//    sc2d::vec<non_trivial_type> non_triv_objs;
+    SECTION("constructor with first, last iterators")
+    {
+        double value = 5.799;
+        sc2d::vec<double> v_1(4, value);
+
+        sc2d::vec<double> v(v_1.begin(), v_1.end());
+        REQUIRE(v.capacity() == 8);
+        REQUIRE(v[0] == 5.799);
+        REQUIRE(v[1] == 5.799);
+        REQUIRE(v[2] == 5.799);
+        REQUIRE(v[3] == 5.799);
+    }
+
+    SECTION("constructor with initialization list")
+    {
+        sc2d::vec<double> v({1.99, 2.99, 3.99, 4.99, 5.99});
+        REQUIRE(v.capacity() == 10);
+        REQUIRE(v[0] == 1.99);
+        REQUIRE(v[1] == 2.99);
+        REQUIRE(v[2] == 3.99);
+        REQUIRE(v[3] == 4.99);
+        REQUIRE(v[4] == 5.99);
+    }
+
+    SECTION("constructor with ref to other vector")
+    {
+
+    }
+
+
+    SECTION("constructor with lvalue to other vector")
+    {
+
+    }
 }
 
