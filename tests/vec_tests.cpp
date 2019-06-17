52cff393ff1ed696eb2a809efa70c261d7d0b9cf
Maksim Ruts <tribar.ge@gmail.com>
2019-06-17 21:26:00 +0300

    Adds initial tests for pool_vector

--- tests/vec_tests.cpp
+++ tests/vec_tests.cpp
@@ -1,12 +1,12 @@
 //
 // Created by maksim.ruts on 3.4.19.
 //
-#include "catch2/catch.hpp"
+#include "doctest/doctest.h"
 #include "../src/memory/pool_allocator.h"
 #include "../src/collections/vec.h"
 
 
-TEST_CASE("vector-operations", "[collections]") {
+TEST_CASE("vector-operations") {
 //    struct trivial_type {
 //        int int_a = 9;
 //        char char_a = 'z';
@@ -30,61 +30,61 @@ TEST_CASE("vector-operations", "[collections]") {
 //    trivial_type trivial_obj;
 //    non_trivial_type non_trivial_obj;
 
-    SECTION("default constructor")
+    SUBCASE("default constructor")
     {
         sc2d::vec<double> v;
-        REQUIRE(v.capacity() == 2);
+        CHECK(v.capacity() == 2);
     }
 
-    SECTION("constructor with size")
+    SUBCASE("constructor with size")
     {
         sc2d::vec<double> v(2);
-        REQUIRE(v.capacity() == 4);
+        CHECK(v.capacity() == 4);
     }
 
-    SECTION("constructor with size, filled with some value")
+    SUBCASE("constructor with size, filled with some value")
     {
         double value = 5.799;
         sc2d::vec<double> v(4, value);
 
-        REQUIRE(v.capacity() == 8);
-        REQUIRE(v[0] == 5.799);
-        REQUIRE(v[1] == 5.799);
-        REQUIRE(v[2] == 5.799);
-        REQUIRE(v[3] == 5.799);
+        CHECK(v.capacity() == 8);
+        CHECK(v[0] == 5.799);
+        CHECK(v[1] == 5.799);
+        CHECK(v[2] == 5.799);
+        CHECK(v[3] == 5.799);
     }
 
-    SECTION("constructor with first, last iterators")
+    SUBCASE("constructor with first, last iterators")
     {
         double value = 5.799;
         sc2d::vec<double> v_1(4, value);
 
         sc2d::vec<double> v(v_1.begin(), v_1.end());
-        REQUIRE(v.capacity() == 8);
-        REQUIRE(v[0] == 5.799);
-        REQUIRE(v[1] == 5.799);
-        REQUIRE(v[2] == 5.799);
-        REQUIRE(v[3] == 5.799);
+        CHECK(v.capacity() == 8);
+        CHECK(v[0] == 5.799);
+        CHECK(v[1] == 5.799);
+        CHECK(v[2] == 5.799);
+        CHECK(v[3] == 5.799);
     }
 
-    SECTION("constructor with initialization list")
+    SUBCASE("constructor with initialization list")
     {
         sc2d::vec<double> v({1.99, 2.99, 3.99, 4.99, 5.99});
-        REQUIRE(v.capacity() == 10);
-        REQUIRE(v[0] == 1.99);
-        REQUIRE(v[1] == 2.99);
-        REQUIRE(v[2] == 3.99);
-        REQUIRE(v[3] == 4.99);
-        REQUIRE(v[4] == 5.99);
+        CHECK(v.capacity() == 10);
+        CHECK(v[0] == 1.99);
+        CHECK(v[1] == 2.99);
+        CHECK(v[2] == 3.99);
+        CHECK(v[3] == 4.99);
+        CHECK(v[4] == 5.99);
     }
 
-    SECTION("constructor with ref to other vector")
+    SUBCASE("constructor with ref to other vector")
     {
 
     }
 
 
-    SECTION("constructor with lvalue to other vector")
+    SUBCASE("constructor with lvalue to other vector")
     {
 
     }
