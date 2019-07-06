aae9c41e8ecefe1c84343047739a6741d0b95bdd
Maksim Ruts <tribar.ge@gmail.com>
2019-07-06 13:28:38 +0300

    Implemets texture atlas

--- src/core/texture_atlas.cpp
+++ src/core/texture_atlas.cpp
@@ -4,15 +4,48 @@
 
 #include "texture_atlas.h"
 
-TextureAtlas::TextureAtlas(unsigned char* img_data, const GLuint width, const GLuint height,
-                           const GLuint img_format)
-{}
-
-void TextureAtlas::bind() const {}
-
-const GLuint& TextureAtlas::get_obj_id() const
+namespace sc2d
 {
-    //    return <#initializer#>;
-}
+    TextureAtlas::TextureAtlas(unsigned char* img_data, const GLuint width, const GLuint height,
+                               const GLuint img_format)
+        : data {img_data}
+        , width {width}
+        , height {height}
+        , internal_format {img_format}
+        , image_format {img_format}
+        , wrap_s {GL_REPEAT}
+        , wrap_t {GL_REPEAT}
+        , filter_min {GL_LINEAR}
+        , filter_max {GL_LINEAR}
+    {
+        generate();
+    }
 
-void TextureAtlas::generate() {}
+    void TextureAtlas::bind() const
+    {
+        glBindTexture(GL_TEXTURE_2D_ARRAY, obj_id);
+    }
+
+    const GLuint& TextureAtlas::get_obj_id() const
+    {
+        return obj_id;
+    }
+
+    void TextureAtlas::generate()
+    {
+        // generate texture
+        glGenTextures(1, &obj_id);
+        // create texture
+        glBindTexture(GL_TEXTURE_2D_ARRAY, obj_id);
+        glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, internal_format, width, height, 0, 0, internal_format,
+                     GL_UNSIGNED_BYTE, data);
+        // set wrap mode
+        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, wrap_s);
+        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, wrap_t);
+        // set filter mode
+        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, filter_min);
+        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, filter_max);
+        // unbind texture
+        glBindTexture(GL_TEXTURE_2D_ARRAY, 0);
+    }
+}
--- src/core/texture_atlas.h
+++ src/core/texture_atlas.h
@@ -13,29 +13,33 @@
 // For now I don't want make any abstract classes, because I don't want to pay for that abstraction
 // or for code branching as Texture2d and TextureAtlas will be used quite often.
 
-class TextureAtlas
+namespace sc2d
 {
-    TextureAtlas() = default;
-    TextureAtlas(unsigned char* img_data, const GLuint width, const GLuint height,
-                 const GLuint img_format);
-    void bind() const;
-    const GLuint& get_obj_id() const;
 
-private:
-    GLuint obj_id;
-    GLuint width;
-    GLuint height;
-    GLuint internal_format;
-    GLuint image_format;
-    GLuint wrap_s;
-    GLuint wrap_t;
-    GLuint filter_min;
-    GLuint filter_max;
-    GLenum texture_type;
+    class TextureAtlas
+    {
+        TextureAtlas() = default;
+        TextureAtlas(unsigned char* img_data, const GLuint width, const GLuint height,
+                     const GLuint img_format);
+        void bind() const;
+        const GLuint& get_obj_id() const;
 
-    unsigned char* data;
+    private:
+        GLuint obj_id;
+        GLuint width;
+        GLuint height;
+        GLuint internal_format;
+        GLuint image_format;
+        GLuint wrap_s;
+        GLuint wrap_t;
+        GLuint filter_min;
+        GLuint filter_max;
+        GLenum texture_type;
 
-    void generate();
-};
+        unsigned char* data;
+
+        void generate();
+    };
+}
 
 #endif //INC_2D_GAME_TEXTURE_ATLAS_H
