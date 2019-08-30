7eaa76ca377da2a31f2a144779a260035594f02d
Maksim Ruts <tribar.ge@gmail.com>
2019-08-30 16:54:14 +0300

    Adds namespace

--- src/core/window.cpp
+++ src/core/window.cpp
@@ -4,19 +4,23 @@
 
 #include "window.h"
 
-Window::Window(const WindowData& window_data, bool isCurrentContext)
-    : window_data(window_data)
+namespace sc2d
 {
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, window_data.context_version_max);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, window_data.context_version_min);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, window_data.opengl_profile);
+
+    Window::Window(const WindowData& window_data, bool isCurrentContext)
+        : window_data(window_data)
+    {
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, window_data.context_version_max);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, window_data.context_version_min);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, window_data.opengl_profile);
 #if __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
 #endif
-    window = glfwCreateWindow(window_data.screen_width, window_data.screen_height,
-                              window_data.window_name.c_str(), nullptr, nullptr);
-    glfwSetFramebufferSizeCallback(window, window_data.frame_buffer_size_callback);
-    glfwSetKeyCallback(window, window_data.key_callback);
-    if(isCurrentContext)
-        glfwMakeContextCurrent(window);
+        window = glfwCreateWindow(window_data.screen_width, window_data.screen_height,
+                                  window_data.window_name, nullptr, nullptr);
+        glfwSetFramebufferSizeCallback(window, window_data.frame_buffer_size_callback);
+        glfwSetKeyCallback(window, window_data.key_callback);
+        if(isCurrentContext)
+            glfwMakeContextCurrent(window);
+    }
 }
--- src/core/window.h
+++ src/core/window.h
@@ -11,30 +11,34 @@
 
 struct GLFWwindow;
 
-struct WindowData
+namespace sc2d
 {
-    int context_version_min;
-    int context_version_max;
-    int opengl_profile;
-    int screen_width;
-    int screen_height;
-    std::string window_name;
-    GLFWframebuffersizefun frame_buffer_size_callback;
-    GLFWkeyfun key_callback;
-};
 
-class Window
-{
-public:
-    Window(const WindowData& window_data, bool isCurrentContext);
-    GLFWwindow* get_window() const
+    struct WindowData
     {
-        return window;
-    }
+        int context_version_min;
+        int context_version_max;
+        int opengl_profile;
+        int screen_width;
+        int screen_height;
+        const char* window_name;
+        GLFWframebuffersizefun frame_buffer_size_callback;
+        GLFWkeyfun key_callback;
+    };
 
-private:
-    WindowData window_data;
-    GLFWwindow* window;
-};
+    class Window
+    {
+    public:
+        Window(const WindowData& window_data, bool isCurrentContext);
+        GLFWwindow* get_window() const
+        {
+            return window;
+        }
+
+    private:
+        WindowData window_data;
+        GLFWwindow* window;
+    };
+}
 
 #endif //INC_2D_GAME_WINDOW_H
