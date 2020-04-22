e1a387f0042fdbf355a6ed470357463cc9826c45
Maksim Ruts <tribar.ge@gmail.com>
2020-04-23 00:04:36 +0300

    Adds limits

---
+++ src/core/limits.h
@@ -0,0 +1,15 @@
+//
+// Created by novasurfer on 4/20/20.
+//
+
+#ifndef SCARECROW2D_LIMITS_H
+#define SCARECROW2D_LIMITS_H
+
+#include <cstddef>
+
+namespace sc2d::limits
+{
+    constexpr size_t MAX_STACK_ARR_SIZE = 1'000'000;
+}
+
+#endif //SCARECROW2D_LIMITS_H
