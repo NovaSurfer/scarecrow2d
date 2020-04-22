3e752913ef6916fe199c88c4d1854b8f225e5603
Maksim Ruts <tribar.ge@gmail.com>
2020-04-23 00:02:53 +0300

    Adds aliases for primitive C/C++ data types

---
+++ src/core/types.h
@@ -0,0 +1,24 @@
+//
+// Created by novasurfer on 4/20/20.
+//
+
+#ifndef SCARECROW2D_TYPES_H
+#define SCARECROW2D_TYPES_H
+
+#include <cstdint>
+
+namespace sc2d
+{
+    using i8 = int8_t;
+    using u8 = uint8_t;
+    using i16 = int16_t;
+    using u16 = uint16_t;
+    using i32 = int32_t;
+    using u32 = uint32_t;
+    using i64 = int64_t;
+    using u64 = uint64_t;
+    using f32 = float;
+    using f64 = double;
+}
+
+#endif //SCARECROW2D_TYPES_H
