9a09bade0bbe6074a928400a9132830596bfc5e3
Maksim Ruts <tribar.ge@gmail.com>
2020-04-27 23:08:28 +0300

    Removes semicolons

--- src/core/dbg/ogl_errors.h
+++ src/core/dbg/ogl_errors.h
@@ -7,8 +7,8 @@
 
 #include "core/log2.h"
 
-#define log_gl_error_cmd() log_err_cmd("%s", sc2d::debug::check_gl_error());
-#define log_gl_error_file() log_err_file("%s", sc2d::debug::check_gl_error());
+#define log_gl_error_cmd() log_err_cmd("%s", sc2d::debug::check_gl_error())
+#define log_gl_error_file() log_err_file("%s", sc2d::debug::check_gl_error())
 
 namespace sc2d::debug
 {
