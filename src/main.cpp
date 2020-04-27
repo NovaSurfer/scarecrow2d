c33c45fbf21cc3d48dc595ad0baadda69b790bff
Maksim Ruts <tribar.ge@gmail.com>
2020-04-27 23:31:03 +0300

    Minor refactioring for main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,54 +5,37 @@
 #include "core/dbg/ogl_errors.h"
 #include "core/log2.h"
 #include "core/result.h"
-#include "core/sprite.h"
-//#include "core/sprite_sheet_inst.h"
-#include "core/event.h"
-#include "core/text_ft2.h"
-#include "core/tiled_map.h"
+#include "core/input.h"
+#include "core/resources.h"
 #include "core/window.h"
-#include "filesystem/configLoader.h"
-#include "filesystem/resourceHolder.h"
+#include "editor/editor_main.h"
+#include "game/game_main.h"
 #include "math/transform.h"
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
-std::unique_ptr<sc2d::Window> window;
-std::unique_ptr<sc2d::Sprite> sprite;
-//std::unique_ptr<sc2d::SpriteSheetInstanced> spritesheet;
+enum class MainMode
+{
+    GAME,
+    EDITOR
+};
 
-sc2d::tiled::Map tiled_map;
-sc2d::TextureAtlas tex_atlas;
-sc2d::Texture2d logo_texture;
-sc2d::Ft2Font fnt_04b_03;
-sc2d::TextFt2 text_ft2;
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
-sc2d::ResultBool engine_init()
+std::unique_ptr<sc2d::Window> window;
+constexpr const sc2d::WindowData window_data {3,
+                                              3,
+                                              GLFW_OPENGL_CORE_PROFILE,
+                                              800,
+                                              600,
+                                              "scarecrow2d",
+                                              framebuffer_size_callback,
+                                              sc2d::Input::read};
+
+sc2d::ResultBool renderer_init()
 {
-    if(!sc2d::Config<sc2d::ResourcesConfigLoad>::open("resources.json"))
-        return sc2d::ResultBool::throw_err(sc2d::Err::RESOURCE_LOADING_FAIL);
 
-    if(!sc2d::Config<sc2d::SceneConfigLoad>::open("data/scenes/zone.json"))
-        return sc2d::ResultBool::throw_err(sc2d::Err::SCENE_LOADING_FAIL);
-
-    return {true};
-}
-
-sc2d::ResultBool program_init()
-{
     glfwInit();
-
-    constexpr const sc2d::WindowData window_data {3,
-                                                  3,
-                                                  GLFW_OPENGL_CORE_PROFILE,
-                                                  800,
-                                                  600,
-                                                  "scarecrow2d",
-                                                  framebuffer_size_callback,
-                                                  key_callback};
     window = std::make_unique<sc2d::Window>(window_data, true);
 
     // Glad loads OpenGL functions pointers
@@ -60,52 +43,15 @@ sc2d::ResultBool program_init()
         return sc2d::ResultBool::throw_err(sc2d::Err::FAILED_TO_INIT_GLAD);
     }
 
-    // Loading engine systems
-    engine_init();
-
     glViewport(0, 0, window_data.screen_width, window_data.screen_height);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_CULL_FACE);
-    //        glFrontFace(GL_CCW);
     glEnable(GL_BLEND);
     glDisable(GL_DEPTH_TEST);
     glDisable(GL_SCISSOR_TEST);
+    log_gl_error_cmd();
 
-    math::mat4 proj =
-        math::ortho(0.0f, static_cast<GLfloat>(window_data.screen_width),
-                    static_cast<GLfloat>(window_data.screen_height), 0.0f, -1.0f, 1.0f);
-
-    // REGULAR SPRITE
-    const sc2d::Shader& sprite_shader = sc2d::ResourceHolder::get_shader("sprite_default");
-    logo_texture = sc2d::ResourceHolder::get_texture("logo");
-    sprite_shader.run();
-    sprite_shader.set_int("image", logo_texture.get_obj_id());
-    sprite_shader.set_mat4("projection", proj);
-    sprite = std::make_unique<sc2d::Sprite>(sprite_shader);
-
-    // SPRITE_SHEEEEEEEEEEET
-    const sc2d::Shader& sprite_sheet_shader = sc2d::ResourceHolder::get_shader("spritesheet");
-    tex_atlas = sc2d::ResourceHolder::get_texture_atlas("tilemap");
-    sprite_sheet_shader.run();
-    sprite_sheet_shader.set_int("image_array", tex_atlas.get_obj_id());
-    log_gl_error_cmd() sprite_sheet_shader.set_mat4("projection", proj);
-    tiled_map = sc2d::ResourceHolder::get_tiled_map("wasd");
-    tiled_map.init(sprite_sheet_shader);
-
-    const sc2d::Shader& font_shader = sc2d::ResourceHolder::get_shader("text_ft2");
-    font_shader.run();
-    font_shader.set_mat4("projection", proj);
-    font_shader.set_vec3("spriteColor", math::vec3(1.f, 0.0f, 1.0f));
-    fnt_04b_03.init("data/fonts/04B_03__.TTF", 48, sc2d::ASCII_TABLE_SIZE);
-    text_ft2.init(font_shader, fnt_04b_03);
-    text_ft2.set_text("hello. I'm Max.");
-    text_ft2.set_pos({100.f, 100.f}, 0.f);
-
-    log_gl_error_cmd()
-
-            return !glGetError()
-        ? (true)
-        : sc2d::ResultBool::throw_err(sc2d::Err::ENGINE_INIT_FAIL);
+    return !glGetError() ? (true) : sc2d::ResultBool::throw_err(sc2d::Err::ENGINE_INIT_FAIL);
 }
 
 void poll_events()
@@ -113,24 +59,22 @@ void poll_events()
     glfwPollEvents();
 }
 
-void draw()
-{
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    tiled_map.draw_map(tex_atlas.get_obj_id());
-    sprite->draw(logo_texture.get_obj_id(), math::vec2(0, 0), math::size2d(111, 148), 0);
-    text_ft2.draw();
-    //    spritesheet->draw(sc2d::ResourceHolder::get_texture_atlas("tilemap"), math::vec2(0, 0),
-    //                     math::size2d(16, 16), 0);
-
-    glfwSwapBuffers(window->get_window());
-}
-
 void update(double dt) { }
 
-int main()
+int main(int argc, char* argv[])
 {
-    program_init();
+    MainMode main_mode = MainMode::GAME;
+    renderer_init();
+    sc2d::Resources::load_all();
+
+    Game game;
+    math::mat4 proj =
+        math::ortho(0.0f, static_cast<GLfloat>(window_data.screen_width),
+                    static_cast<GLfloat>(window_data.screen_height), 0.0f, -1.0f, 1.0f);
+
+    game.init(GameMode::SCENE, proj);
+    glfwSetWindowUserPointer(window->get_window(), &game);
+
 
     // TODO: Make class for time management
     double begin_ticks = glfwGetTime();
@@ -141,8 +85,14 @@ int main()
     while(!glfwWindowShouldClose(window->get_window())) {
         update(delta_time);
         poll_events();
-        draw();
-
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        if(main_mode == MainMode::GAME) {
+            game.draw();
+        } else {
+            Editor::draw();
+        }
+        glfwSwapBuffers(window->get_window());
         end_ticks = glfwGetTime();
         delta_time = (begin_ticks - end_ticks) / glfwGetTime();
 
@@ -156,17 +106,10 @@ int main()
         begin_ticks = end_ticks;
     }
 
-    text_ft2.destroy();
+    game.destroy();
+    sc2d::Resources::clean_all();
     glfwTerminate();
     return 0;
-}
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
-    // When a user presses the escape key, we set the WindowShouldClose property to true, closing the application
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GL_TRUE);
-    }
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
