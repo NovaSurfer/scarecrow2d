3f852811c7fc1e0c0729aa5e4df9dedc7d2beadb
Maksim Ruts <tribar.ge@gmail.com>
2020-04-27 23:28:21 +0300

    Adds editor mode (WIP)

---
+++ src/editor/editor_main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by novasurfer on 4/23/20.
+//
+
+#include "editor_main.h"
+#include "core/log2.h"
+
+void Editor::draw()
+{
+    log_info_cmd("drawing editor");
+}
---
+++ src/editor/editor_main.h
@@ -0,0 +1,13 @@
+//
+// Created by novasurfer on 4/23/20.
+//
+
+#ifndef SCARECROW2D_EDITOR_MAIN_H
+#define SCARECROW2D_EDITOR_MAIN_H
+
+struct Editor
+{
+    static void draw();
+};
+
+#endif //SCARECROW2D_EDITOR_MAIN_H
