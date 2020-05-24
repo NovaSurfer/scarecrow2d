127c95cb27ab997d5ee749e551852c3070817916
Maksim Ruts <tribar.ge@gmail.com>
2020-05-24 17:16:39 +0300

    Minor refactoring

--- src/game/game_main.cpp
+++ src/game/game_main.cpp
@@ -3,12 +3,13 @@
 //
 
 #include "game_main.h"
+#include "core/camera.h"
 #include "core/input_types.h"
 #include "core/log2.h"
+#include "core/texture.h"
 #include "core/window.h"
 #include "filesystem/resourceHolder.h"
 #include "menu.h"
-#include "core/camera.h"
 
 GameMode Game::mode;
 
@@ -18,11 +19,12 @@ sc2d::ResultBool Game::init(GameMode start_mode, const sc2d::WindowSize& window_
 
     ///------------- INIT TEST SCENE / LEVEL
     sc2d::Camera camera;
-    camera.make_orthographic(window_size.width, window_size.height, sc2d::DEFAULT_Z_NEAR, sc2d::DEFAULT_Z_FAR);
+    camera.make_orthographic(window_size.width, window_size.height, sc2d::DEFAULT_Z_NEAR,
+                             sc2d::DEFAULT_Z_FAR);
 
     // REGULAR SPRITE
     const sc2d::Shader& sprite_shader = sc2d::ResourceHolder::get_shader("sprite_default");
-    logo_texture = sc2d::ResourceHolder::get_texture("logo");
+    const sc2d::Texture2d logo_texture = sc2d::ResourceHolder::get_texture("logo");
 
     // TODO: send MVP matrix at start, remove this set_projection.
     sprite_shader.run();
@@ -34,21 +36,22 @@ sc2d::ResultBool Game::init(GameMode start_mode, const sc2d::WindowSize& window_
 
     // SPRITE_SHEEEEEEEEEEET
     const sc2d::Shader& sprite_sheet_shader = sc2d::ResourceHolder::get_shader("spritesheet");
-    tex_atlas = sc2d::ResourceHolder::get_texture_atlas("tilemap");
+    const sc2d::TextureAtlas tex_atlas = sc2d::ResourceHolder::get_texture_atlas("tilemap");
     sprite_sheet_shader.run();
-    sprite_sheet_shader.set_int("image_array", tex_atlas);
     sprite_sheet_shader.set_mat4("projection", camera.get_proj());
     tiled_map = sc2d::ResourceHolder::get_tiled_map("wasd");
     tiled_map.init(sprite_sheet_shader);
+    tiled_map.set_sheet_texture(tex_atlas);
 
     const sc2d::Shader& font_shader = sc2d::ResourceHolder::get_shader("text_ft2");
     font_shader.run();
     font_shader.set_mat4("projection", camera.get_proj());
-    font_shader.set_vec3("spriteColor", math::vec3(1.f, 0.0f, 1.0f));
+    sc2d::Ft2Font fnt_04b_03;
     fnt_04b_03.init("data/fonts/04B_03__.TTF", 48, sc2d::ASCII_TABLE_SIZE);
     text_ft2.init(font_shader, fnt_04b_03);
     text_ft2.set_text("hello. I'm Max.");
-    text_ft2.set_pos({100.f, 100.f}, 0.f);
+    text_ft2.set_pos({100.f, 100.f}, 0);
+    text_ft2.set_color(sc2d::Color::CYAN);
 
     log_gl_error_cmd();
 
@@ -60,7 +63,7 @@ void Game::draw()
     if(mode == GameMode::MENU) {
         menu.draw();
     } else {
-        tiled_map.draw_map(tex_atlas.get_obj_id());
+        tiled_map.draw_map();
         sprite.draw();
         text_ft2.draw();
         //    spritesheet->draw(sc2d::ResourceHolder::get_texture_atlas("tilemap"), math::vec2(0, 0),
--- src/game/game_main.h
+++ src/game/game_main.h
@@ -6,7 +6,6 @@
 #define SCARECROW2D_GAME_MAIN_H
 
 #include "core/text_ft2.h"
-#include "core/texture.h"
 #include "core/tiled_map.h"
 #include <core/sprite.h>
 #include "core/result.h"
@@ -36,9 +35,6 @@ private:
     Menu menu;
     sc2d::Sprite sprite;
     sc2d::tiled::Map tiled_map;
-    sc2d::TextureAtlas tex_atlas;
-    sc2d::Texture2d logo_texture;
-    sc2d::Ft2Font fnt_04b_03;
     sc2d::TextFt2 text_ft2;
 };
 
