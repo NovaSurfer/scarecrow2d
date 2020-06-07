fa66e7c5191f7b9e612891e03ce941412a9a6cb0
Maksim Ruts <tribar.ge@gmail.com>
2020-06-07 17:30:26 +0300

    Minor improvements

--- src/core/dbg/ogl_errors.cpp
+++ src/core/dbg/ogl_errors.cpp
@@ -10,24 +10,24 @@ namespace sc2d::debug
     const char* check_gl_error()
     {
         GLenum err;
-        const char* err_str = "GL_NO_ERROR";
+        const char* err_str = nullptr;
         while((err = glGetError()) != GL_NO_ERROR) {
             switch(err) {
-                case GL_INVALID_ENUM:
-                    err_str = "GL_INVALID_ENUM";
-                    break;
-                case GL_INVALID_VALUE:
-                    err_str = "GL_INVALID_VALUE";
-                    break;
-                case GL_INVALID_OPERATION:
-                    err_str = "GL_INVALID_OPERATION";
-                    break;
-                case GL_OUT_OF_MEMORY:
-                    err_str = "GL_OUT_OF_MEMORY";
-                    break;
-                case GL_INVALID_FRAMEBUFFER_OPERATION:
-                    err_str = "GL_INVALID_FRAMEBUFFER_OPERATION";
-                    break;
+            case GL_INVALID_ENUM:
+                err_str = "GL_INVALID_ENUM";
+                break;
+            case GL_INVALID_VALUE:
+                err_str = "GL_INVALID_VALUE";
+                break;
+            case GL_INVALID_OPERATION:
+                err_str = "GL_INVALID_OPERATION";
+                break;
+            case GL_OUT_OF_MEMORY:
+                err_str = "GL_OUT_OF_MEMORY";
+                break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION:
+                err_str = "GL_INVALID_FRAMEBUFFER_OPERATION";
+                break;
             }
         }
         return err_str;
