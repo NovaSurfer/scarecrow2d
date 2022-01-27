0a5a2a68efde93cd5c9abba5fe56bb1f02a9ce3f
Maksim Ruts <tribar.ge@gmail.com>
2022-01-27 22:45:39 +0300

    Updates shaders projection matrix on window resize

--- sc2d/src/core/glfw_base/glfwbase.cpp
+++ sc2d/src/core/glfw_base/glfwbase.cpp
@@ -4,6 +4,7 @@
 
 #include "glfwbase.h"
 #include <GLFW/glfw3.h>
+#include <filesystem/resourceHolder.h>
 
 namespace sc2d
 {
@@ -19,6 +20,9 @@ namespace sc2d
             // make sure the viewport matches the new window dimensions; note that width and
             // height will be significantly larger than specified on retina displays.
             glViewport(0, 0, width, height);
+            // TODO: update projection for the camera inside game
+            // TODO: left the position of an object on the same place
+            sc2d::ResourceHolder::update_shaders_proj(math::ortho(0.0, width, height, 0.0, DEFAULT_Z_NEAR, DEFAULT_Z_FAR));
         }
     }
 }
